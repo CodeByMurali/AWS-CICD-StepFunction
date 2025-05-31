95c6f9416f295d0206f69e8ce32e6b3bd6f0a0d1
murali.rajendran <murali.rajendran@veeva.com>
2025-05-31 15:51:00 -0400

    Modified start and stop script

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull murali16394/simple-python-flask-app:latest
+
+# Run the Docker image as a container
+echo
+docker run -d --name simple-python-flask-app -p 5000:5000 murali16394/simple-python-flask-app:latest
---
+++ scripts/stop_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Container name or partial match
+CONTAINER_NAME="simple-python-flask-app"
+
+# Find container ID(s) of any running container matching the name
+RUNNING_CONTAINER_ID=$(docker ps -q --filter "name=$CONTAINER_NAME")
+
+if [ -n "$RUNNING_CONTAINER_ID" ]; then
+  echo "Stopping running container(s) matching '$CONTAINER_NAME'..."
+  docker stop $RUNNING_CONTAINER_ID
+else
+  echo "No running container found matching '$CONTAINER_NAME'."
+fi
