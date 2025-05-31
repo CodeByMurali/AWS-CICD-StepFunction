15c569f681d46d8c80d5ecbf01de958b21541b0a
Murali <165854672+CodeByMurali@users.noreply.github.com>
2025-05-31 18:49:28 -0400

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -4,13 +4,13 @@ set -e
 # Container name or partial match
 CONTAINER_NAME="simple-python-flask-app"
 
-# Find container ID(s) of any running container matching the name
-RUNNING_CONTAINER_ID=$(docker ps -aq --filter "name=$CONTAINER_NAME")
+# Find all running container IDs matching the name
+RUNNING_CONTAINER_IDS=$(docker ps -aq --filter "name=$CONTAINER_NAME")
 
-if [ -n "$RUNNING_CONTAINER_ID" ]; then
-  echo "Stopping running container(s) matching '$CONTAINER_NAME'..."
-  docker stop $RUNNING_CONTAINER_ID
-  docker rm $RUNNING_CONTAINER_ID
+if [ -n "$RUNNING_CONTAINER_IDS" ]; then
+  echo "Stopping and removing container(s) matching '$CONTAINER_NAME'..."
+  docker stop $RUNNING_CONTAINER_IDS
+  docker rm $RUNNING_CONTAINER_IDS
 else
   echo "No running container found matching '$CONTAINER_NAME'."
 fi
