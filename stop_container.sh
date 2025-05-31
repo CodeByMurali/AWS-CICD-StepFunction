c5565bb59f6a5c0195a0e53f15a172d3afc48850
Murali <165854672+CodeByMurali@users.noreply.github.com>
2025-05-31 18:48:52 -0400

    Remove stopped contianer

--- stop_container.sh
+++ stop_container.sh
@@ -10,6 +10,7 @@ RUNNING_CONTAINER_ID=$(docker ps -aq --filter "name=$CONTAINER_NAME")
 if [ -n "$RUNNING_CONTAINER_ID" ]; then
   echo "Stopping running container(s) matching '$CONTAINER_NAME'..."
   docker stop $RUNNING_CONTAINER_ID
+  docker rm $RUNNING_CONTAINER_ID
 else
   echo "No running container found matching '$CONTAINER_NAME'."
 fi
